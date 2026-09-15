83da1d5e226b2f750baafe6d57d555ad25249bfa
inregis <inregis1@gmail.com>
2026-09-14 22:36:15 -0300

    Atualização de aulas

---
+++ a08/08.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}Km`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
